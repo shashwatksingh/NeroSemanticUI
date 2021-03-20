4063b283ae95f062c5fa3fd03783b6a4f0fcae03
harryalbrus272 <shashwatksingh.27@gmail.com>
2021-03-20 12:06:58 +0530

    improved path logic and routing

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
-import { Menu, Segment } from "semantic-ui-react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const MenuBar = () => {
-  const [activeItem, setActiveItem] = useState("");
+  //path speicific switching
+  const pathname = window.location.pathname;
+  //about the path
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
+
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
     <div>
-      <Menu pointing secondary>
+      <Menu pointing secondary size="massive" color="teal">
         <Menu.Item
           name="home"
           active={activeItem === "home"}
           onClick={handleItemClick}
+          as={Link}
+          to="/"
         />
         <Menu.Menu position="right">
           <Menu.Item
             name="login"
             active={activeItem === "login"}
             onClick={handleItemClick}
+            as={Link}
+            to="/login"
           />
           <Menu.Item
             name="register"
             active={activeItem === "register"}
             onClick={handleItemClick}
+            as={Link}
+            to="/"
           />
         </Menu.Menu>
       </Menu>
